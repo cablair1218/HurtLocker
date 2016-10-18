9cbd65c5de47524285523b5066a28704019567f1
Carina Blair <carinablair@Carina-ZCW-5.local>
2016-10-18 08:49:17 -0400

    Added error message count and formatting

--- src/main/java/blair/carina/HurtLocker/ParseData.java
+++ src/main/java/blair/carina/HurtLocker/ParseData.java
@@ -8,44 +8,72 @@ import java.util.regex.Pattern;
 public class ParseData {
 
     private String output = "";
+    private String[] groceryObjects;
     private ArrayList<GroceryItem> groceryItems = new ArrayList<>();
     private ArrayList<String> foodNames = new ArrayList<>();
 
 
     public String outputText(String str){
-        parseInfo(str);
+        jerkSonParse(str);
         for (GroceryItem groceryItem : groceryItems) {
             String name = formatNamePrint(groceryItem);
             String price = formatPricePrint(groceryItem);
             String total = formatTotalItems(groceryItem);
             output += name + total + "\n=============\t\t=============\n"+ price +"\n";
         }
+        String error = formatError(calculateError());
+        output+= error;
         return output;
 
     }
 
-    private void parseInfo(String str){
-        String parse = "((?i)\\bname:\\b)(\\w+)([;@^*%!])(\\b(?i)price:\\b)(\\d.\\d{2})([;@^*%!])((?i)\\btype:\\b)(\\w+)([;@^*%!])(\\b(?i)expiration:\\b)(\\d/\\d{2}/\\d{4})(##)";
-        Pattern pattern = Pattern.compile(parse);
-        Matcher matcher = pattern.matcher(str);
-        while(matcher.find()){
-            String name = matcher.group(2);
-            name = formatName(name);
-            if(foodNames.contains(name)) {
-                duplicateItems(name, matcher.group(5));
+    private void jerkSonParse(String str){
+        groceryObjects = str.split("##");
+        parseInfo(groceryObjects);
+    }
+
+    private void parseInfo(String[] arrGroceryObjects){
+        for (String s: arrGroceryObjects) {
+            String parse = "((?i)\\bname:\\b)(\\w+)([;@^*%!])(\\b(?i)price:\\b)(\\d.\\d{2})([;@^*%!])((?i)\\btype:\\b)(\\w+)([;@^*%!])(\\b(?i)expiration:\\b)(\\d/\\d{2}/\\d{4})";
+            Pattern pattern = Pattern.compile(parse);
+            Matcher matcher = pattern.matcher(s);
+            while(matcher.find()){
+                String name = matcher.group(2);
+                name = formatName(name);
+                if(foodNames.contains(name)) {
+                    duplicateItems(name, matcher.group(5));
+                }
+                else {
+                    GroceryItem item = new GroceryItem(name, matcher.group(5), matcher.group(8), matcher.group(11));
+                    groceryItems.add(item);
+                    foodNames.add(name);
+                    item.getPriceCount().put(matcher.group(5),1);
+                }
             }
-            else {
-                GroceryItem item = new GroceryItem(name, matcher.group(5), matcher.group(8), matcher.group(11));
-                groceryItems.add(item);
-                foodNames.add(name);
-                item.getPriceCount().put(matcher.group(5),1);
-            }
+
         }
     }
 
     private String formatName(String name){
         name = name.replaceAll("\\d+","o");
         return name.substring(0,1).toUpperCase() + name.substring(1).toLowerCase();
+    }
+
+    private int calculateError(){
+        int totalItems = 0;
+        for(GroceryItem g: groceryItems){
+            for(int i : g.getPriceCount().values()){
+                totalItems += i;
+            }
+
+        }
+        int error = groceryObjects.length - totalItems;
+        return error;
+    }
+
+    private String formatError(int error){
+        return String.format("Errors\t\t\t\tseen: %d times", error);
+
     }
 
     private String formatTotalItems(GroceryItem groceryItem){
@@ -90,9 +118,4 @@ public class ParseData {
             }
         }
     }
-
-
-
-
-    public class MissingValueException extends Exception{}
 }
